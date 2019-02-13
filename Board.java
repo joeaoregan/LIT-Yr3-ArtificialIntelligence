a3df8c593fd9d89d416709401cafb2c7682f3b0d
Joe O'Regan <joeaoregan@yahoo.ie>
2019-02-13 20:36:22 +0000

    20170507 changes

--- Board.java
+++ Board.java
@@ -1,31 +1,405 @@
+import java.util.Iterator;
+
+
+/*
+ * Joe O'Regan
+ * K00203642
+ */
+
 public class Board{
+	int neighboursCalled = 0;
+	int N = 0; 												// Size for N x N board
+	
+	int[][] solutionBoard = {{1,2,3},{4,5,6},{7,8,0}};		// Board to reach
+	int[][] board;											// Board we have
+	int[][] iterableBoard;
+	Queue<Board> allBoards = new Queue<>();
+	
     public Board(int [][] tiles){
         // YOUR CODE HERE
+    	N = tiles.length;						// Set the value of N, the dimension of the board
+       
+    	//System.out.println("Board N: " + N);
+        
+    	board = new int[N][N];					// Initialise board (causes error otherwise)
+    	
+	    for (int i = 0; i < N; i++){
+	    	for (int j = 0; j < N; j++) {
+	    		board[i][j] = tiles[i][j];		// Set board array equal to tiles
+	    	}
+	    }
     }
     
     public int hamming(){
         // YOUR CODE HERE
-        return 0;
+    	//int[][] solutionBoard = {{1,2,3},{4,5,6},{7,8,0}};	// All tiles in correct position
+    	int outOfPosition = 0;								// Number of blocks in the wrong position
+    	//int movesSoFar = 0;									// Number of moves made so far
+    	
+	    for (int i = 0; i < N; i++){
+	    	for (int j = 0; j < N; j++) {
+	    		//if(solutionBoard[i][j] == boardToCheck[i][j]) System.out.println("board[" + i + "][" + j + "] equal");
+	    		//else System.out.println("board[" + i + "][" + j + "] not equal");	    
+	    		if (solutionBoard[i][j] != board[i][j] && board[i][j] != 0) outOfPosition++;											// Increment the number of tiles out of position
+	    	}	    	
+	    }
+	    
+        return outOfPosition;
     }
     
     public int manhattan(){
         // YOUR CODE HERE
-        return 0;
+    	int x = 0, y = 0, manhattanTotal = 0;				// Init variables
+    	
+	    for (int i = 0; i < N; i++){
+	    	for (int j = 0; j < N; j++) {
+	    		//  		
+	    	    for (int k = 0; k < N; k++){
+	    	    	for (int l = 0; l < N; l++) {
+    	    			if(solutionBoard[i][j] != board[i][j]){
+		    	    		if(solutionBoard[i][j] == board[k][l] && solutionBoard[i][j] != 0) {
+	    	    				//System.out.println("board[" + i + "][" + j + "] equal");
+
+	    	    				//x = abs(k - i);
+	    	    				//y = abs(l - j);
+	    	    				x = Math.abs(l - j);	// Number of moves left or right
+	    	    				y = Math.abs(k - i);	// Number of moves up or down
+	    	    				
+	    	    				manhattanTotal += x + y;	    	    				
+	    	    				
+	    	    				//System.out.println(solutionBoard[i][j] + " X: " + x + " Y: " + y);
+	    	    			}
+		    	    		//else System.out.println("board[" + i + "][" + j + "] not equal");
+    	    			}
+	    	    	}
+	    	    }	    	    
+	    	}
+	    }    	
+	    
+        return manhattanTotal;
+    }
+    
+    public boolean boardsAreEqual(int[][] boardA, int[][] boardB){
+	    for (int i = 0; i < N; i++){
+	    	for (int j = 0; j < N; j++) {
+	    		if(boardA[i][j] != boardB[i][j]) return false;	// If one any tile is out of place the layout is not the same
+	    		//System.out.println("Boards are equal");
+	    	}
+    		//System.out.println("Boards are NOT equal");
+	    }
+	    
+	    return true;											// Otherwise the boards have the same tile layout
+    }
+    
+    public boolean boardIsSolution(){
+    	
+	    for (int i = 0; i < N; i++){
+	    	for (int j = 0; j < N; j++) {
+	    		if(board[i][j] != solutionBoard[i][j]) return false;	// If one any tile is out of place the layout is not the same
+	    	}
+	    }
+	    
+    	//System.out.println("Board is solution");
+	    
+	    return true;											// Otherwise the boards have the same tile layout
     }
     
     public boolean equals( Object o){
         // YOUR CODE HERE
-        return false;
+    	/*
+    	// Date and Transaction examples off Princeton FAQ
+    	
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+        Transaction that = (Transaction) other;
+        return (this.amount == that.amount) && (this.who.equals(that.who))
+                                            && (this.when.equals(that.when));
+    	
+
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+        Date that = (Date) other;
+        return (this.month == that.month) && (this.day == that.day) && (this.year == that.year);
+        */
+
+    	
+        if (o == this) return true;
+        if (o == null) return false;
+        if (o.getClass() != this.getClass()) return false;
+        Board that = (Board) o;
+        return (this.N == that.N) && boardsAreEqual(this.board, that.board);	// if boards have the same dimensions, and tile layout
+    	
+        //return false; // Unreachable
+    }
+        
+    // Make board A the same as board B
+    //public void copyBoard(Board boardA, Board boardB){
+    public void copyBoard(int[][] boardA, int[][] boardB) {
+        for (int i = 0; i < N; i++){
+        	for (int j = 0; j < N; j++) {
+        		//boardA.board[i][j] = boardB.board[i][j];	// make boards the same
+        		boardA[i][j] = boardB[i][j];
+        	}
+        }
+    }
+
+    //public Iterable<Board> neighbours(Queue<Board> q){  
+    public Iterable<Board> neighbours(){  
+    	neighboursCalled++;
+    	System.out.println("Neighbours: " + neighboursCalled);
+    //public Iterable<Board> neighbours(){    	
+        // YOUR CODE HERE   
+		//MaxPQ<Board> boardList = new MaxPQ();
+    	//MaxPQ<Board> boardList = new MaxPQ<Board>();
+    	//MinPQ<Board> boardQueue = new MinPQ<Board>(10);							// Need to set size for priority queue
+    	//MinPQ<State> stateQueue = new MinPQ<State>();
+    	//System.out.println("size of boardQueue: " + boardQueue.size(10));
+    	//private Key[] key;													// Set a key for the priority queue (hamming or manahattan)
+    	
+    	//Board tempBoard = new Board();
+    	//int[][] temp = 
+    	// Make iterable board = board
+    	/*
+        for (int i = 0; i < 3; i++){
+        	for (int j = 0; j < 3; j++) {
+        		iterableBoard[i][j] = board[i][j];	// make boards the same
+        	}
+        }
+        */
+    	//System.out.println("Iterable Board:\n");
+    	//copyBoard(iterableBoard, board);
+    	//System.out.println(iterableBoard.toString());
+    	
+    	Queue<Board> boardQueue = new Queue<>();
+    	
+    	int temp;																// Hold blank tiles old board position  	
+    	
+	    for (int y = 0; y < N; y++){
+	    	for (int x = 0; x < N; x++) {
+	    		if (board[y][x] == 0){	// Find the blank tile
+	    			// First Check for able to move blank tile left or right	
+	    			//int[][] tempLayout = new int[N][N];	    			
+	    			int[][] b1 = new int[N][N];
+	    			int[][] b2 = new int[N][N];
+	    			int[][] b3 = new int[N][N];
+	    			int[][] b4 = new int[N][N];
+	    			
+	    			copyBoard(b1, board);
+	    			copyBoard(b2, board);
+	    			copyBoard(b3, board);
+	    			copyBoard(b4, board);
+	    			
+	    			//System.out.println("N: " + N);	    			
+	    			
+	    			if (x > 0) {												// Can move blank tile left
+	    				//System.out.println("\nMove Left:");
+	    				temp = b1[y][x];										// Swap the tiles position
+	    				b1[y][x] = b1[y][x - 1];
+	    				b1[y][x - 1] = temp;
+	    				
+	    				Board board1 = new Board(b1);
+	    				//if(checkBoard(board1, q)) {
+		    			if(checkBoard(board1)) {
+	    					boardQueue.enqueue(board1);
+		    				System.out.println("\nMove Left:");
+		    				System.out.println(board1.toString());
+	    				}
+
+	    				//System.out.println("board1 hamming: " + board1.hamming());
+	    				//System.out.println("board1 manhattan: " + board1.manhattan());
+
+	    		        //key = (Key[]) new Comparable[board1.manhattan()];
+	    		        
+	    				//boardQueue.insert(board1);
+	    		    	//System.out.println("size of boardQueue: " + boardQueue.size());
+	    				//System.out.println(board1.toString());
+	    				
+	    				//System.out.println("test");
+	    				//System.out.println(boardQueue[0].toString());
+
+	    				//System.out.println("delMin() index: " + boardQueue.delMin());
+	    				//System.out.println(boardQueue.delMin().toString());		// Dequeue the board with the minimum priority
+
+	    				//boardQueue.enqueue(board1);
+	    				//System.out.println("boardQueue size: " + boardQueue.size());
+	    			}	    			
+	    			if (x < N - 1) {												// Can move blank tile right (change to N-1, out of bounds error)
+	    				//System.out.println("\nMove Right:");
+	    				temp = b2[y][x];
+	    				b2[y][x] = b2[y][x + 1];
+	    				b2[y][x + 1] = temp;
+
+	    				//Board board0 = new Board(board);
+	    				Board board2 = new Board(b2);
+	    		    	//System.out.println("size of boardQueue: " + boardQueue.size());
+	    				//System.out.println("Board 0");
+	    				//System.out.println(board2.toString());
+
+	    				//if(checkBoard(board2, q)) {
+		    			if(checkBoard(board2)) {
+	    					boardQueue.enqueue(board2);
+		    				System.out.println("\nMove Right:");
+		    				System.out.println(board2.toString());
+	    				}
+	    			}	    			
+	    			if (y > 0) {												// Can move blank tile up
+	    				//System.out.println("\nMove Up:");
+	    				temp = b3[y][x];
+	    				b3[y][x] = b3[y - 1][x];
+	    				b3[y - 1][x] = temp;
+	    				
+	    				Board board3 = new Board(b3);
+	    				//System.out.println(board3.toString());
+	    				//if(checkBoard(board3, q)) {
+		    			if(checkBoard(board3)) {
+	    					boardQueue.enqueue(board3);
+		    				System.out.println("\nMove Up:");
+		    				System.out.println(board3.toString());
+	    				}  				
+	    			}	    			
+	    			if (y < N - 1) {												// Can move blank tile down (change to N-1, out of bounds error)
+	    				//System.out.println("\nMove Down:");
+	    				//System.out.println("Tile Down - y: " + y + " x: " + x);
+	    				temp = b4[y][x];
+	    				b4[y][x] = b4[y + 1][x];
+	    				b4[y + 1][x] = temp;
+	    				
+	    				Board board4 = new Board(b4);
+	    				//System.out.println(board4.toString());
+	    				
+	    				//testBoard(board4);
+	    				//if(checkBoard(board4, q)) {
+		    			if(checkBoard(board4)) {
+	    					boardQueue.enqueue(board4);
+		    				System.out.println("\nMove Down:");
+		    				System.out.println(board4.toString());
+	    				}
+	    			}
+	    			
+    				System.out.println("size of boardQueue " + boardQueue.size());
+	    		}
+	    	}
+	    }
+	    
+	    
+	    //System.out.println(boardQueue.dequeue().toString());
+
+        return boardQueue;
+    }
+
+    //public boolean checkBoard(Board b, Queue<Board> allBoards){
+    public boolean checkBoard(Board b){
+    	//boolean putOnQueue = false;
+		if (allBoards.isEmpty()) {							// If the queue of all previous boards is empty, board can be added
+			return true;
+		}
+		else {
+	    	//Queue<Board> checkBoards = allBoards;
+    		Queue<Board> checkBoards = new Queue<>();
+    		Iterator<Board> it = allBoards.iterator();
+    		while(it.hasNext()){
+    			checkBoards.enqueue(it.next());		   		// Deep copy the queue     			
+    		}
+    		
+    		
+	    	while (!checkBoards.isEmpty()){
+	    		Board checkThis = checkBoards.dequeue();	// check each board on the queue
+	    		
+	    		if(boardsAreEqual(checkThis.board, b.board)) {// if they are equal (already on the queue)
+	    			//putOnQueue = false;
+	    			return false;							// return false, and don't bother adding it	    			
+	    		}
+	    		//else putOnQueue = true;
+	    	}
+			//if (putOnQueue) 
+				allBoards.enqueue(b);						// Add the board to the previously visited boards queue
+		}
+		
+    	return true;										// if the board is not already on the boards list, return true to add it
     }
     
-    public Iterable<Board> neighbours(){
-        // YOUR CODE HERE
-        return null;
+    public void testBoard(Board b){
+		System.out.println("Current board hamming: " + b.hamming());
+		System.out.println("Current board manhattan: " + b.manhattan());
+		System.out.println(b.toString());
     }
     
     public String toString(){
-        // YOUR CODE HERE
-        return "";
+        // YOUR CODE HERE    
+    	String boardStr = "";									// Convert the board to a string for output
+    	
+    	for (int i = 0; i < N; i++){
+	    	for (int j = 0; j < N; j++){
+	    		//System.out.print(" " + board[i][j]);
+	    		//if (j<2) System.out.print(" |");
+	    		boardStr += " " + board[i][j];
+	    		//if (j < N - 1) boardStr += (" |");
+	    	}
+	    	/*
+			//if (i<2) System.out.println("\n---|---|---");
+			//else System.out.println("\n   |   |   ");
+			//if (i<2) boardStr += "\n---|---|---\n";
+	    	if (i < N - 1) {
+		    	for (int k = 0; k < N - 1; k++){
+		    		if (k == 0) boardStr += "\n---|";
+		    		if (k < N-2) boardStr += "---|";
+		    		else if (k < N-1) boardStr += "---";
+		    	}
+	    	}
+	    	*/
+	    	if (i < N - 1) boardStr += "\n";
+    	}
+    	//System.out.println("\n");
+    	boardStr += "\n";
+    	    	
+        return boardStr;										// String representation of the board
+    }
+    
+    public static void main(String [] args){
+        
+        int[][] test1 = {{8,1,3},{4,0,2},{7,6,5}};	// 5 out of position (blank tile not included)
+
+        int[][] test2 = {{1,2,3},{4,5,6},{7,8,0}};	// 0 out of position
+        
+        //System.out.println(" x = 3, y = 2 test1[2][3]: " + test1[1][2]); // Test board[y][x] positions for x and y, i = y, j = x
+
+      //  MinPQ<Board> pq = new MinPQ<Board>();
+        
+		Board board1 = new Board(test1);
+		Board board2 = new Board(test2);
+		
+		//pq.insert(board1);
+		//pq.insert(board2);
+		
+		//int size = pq.size();	// error
+		
+		//System.out.println("MinPQ size: " + size);	// error
+		
+		/*
+		//Board board3 = new Board({{1,2,3},{4,5,6},{7,8,0}});
+		//Board board3 = new Board({1,2,3,4,5,6,7,8,0});
+		//Board board3 = new Board(1,2,3,4,5,6,7,8,0);
+		
+       // board1.hamming(test1);
+        //board2.hamming(test2);    
+        //board1.toString();
+        //board2.toString();
+		*/
+		
+        System.out.println(board1.toString());
+        System.out.println(board2.toString());
+        //System.out.println("Board 1 Hamming: " + board1.hamming());
+        //System.out.println("Board 2 Hamming: " + board2.hamming());
+        
+        //System.out.println("Board 1 Manhattan Distance: " + board1.manhattan());
+        
+        //board1.copyboard(board1,board2);
+       // System.out.println(board1.toString());
+        //System.out.println(board2.toString());
+        
     }
 }
+
         
--- Solver.java
+++ Solver.java
@@ -1,25 +1,212 @@
-public class Solver{
-    public Solver(Board initial){
+import java.util.Comparator;
+import java.util.*;
+
+public class Solver {
+	MinPQ<State> statePQ;
+	Queue<State> doneQ = new Queue<>();
+	int minNumMoves = 0;
+	State solutionState;
+	// Moved from Board class
+	//Queue<Board> allBoardsQ = new Queue<Board>();	// store all boards visited on a queue
+	//Queue<Board> allBoardsQ = new Queue<>();	// store all boards visited on a queue
+	
+	private class State implements Comparator<State>, Comparable<State>{
+		Board board;
+		int moves;
+		State prevState;
+		//boolean visited = false;
+		
+		public State(Board b, State s) {
+			board = b;
+			//moves = moves();
+			moves = 0;
+			prevState = s;
+		}
+
+		// Overriding the compareTo method (https://www.tutorialspoint.com/java/java_using_comparator.htm)
+		public int compareTo(State s1) {
+		      //return (this.prevState.board.manhattan()).compareTo(s1.prevState.board.manhattan());	// gives dereferenced error
+		      //return (this.prevState).compareTo(s1.prevState);	// gives dereferenced error
+		      return 0;
+		}
+		
+		// Overriding the compare method to sort by manhattan() function
+		public int compare(State s1, State s2) {
+			return s1.board.manhattan() - s2.board.manhattan();
+		}
+	}
+	
+    public Solver(Board initial) {
+    	State tempState;    	
+    	statePQ = new MinPQ<State>();
+    	boolean foundSolution = false;
+    	
+    	//Queue<State> doneQ = new Queue<>();
+    	
+    	State initialState = new State(initial, null);   
+    	initialState.moves = 0;
+
+    	//doneQ.enqueue(initialState);
+    	//doneQ.enqueue(initialState);
+    	doneQ.enqueue(initialState);
+    	//System.out.println("doneQ " + doneQ.size());// not adding more than 1 to queue
+    	//allBoardsQ.enqueue(initialState.board);	// add initial board to queue of boards    	
+    	//allBoardsQ.enqueue(initialState.board);	// add initial board to queue of boards
+    	//allBoardsQ.enqueue(initialState.board);	// add initial board to queue of boards CHECK ADDING TO QUEUE
+    	//System.out.println("allB " + allBoardsQ.size());// not adding more than 1 to queue
+    	
+    	statePQ.insert(initialState);
+    	
+        while(!foundSolution) {
+        	tempState = statePQ.delMin();														// Get a state from the queue
+        	
+        	if (tempState.board.boardIsSolution()) {											// if the state is the solution
+        		solutionState = tempState;														// set the solution state to as the current state
+        		foundSolution = true;															// finished / exit function
+        	}
+			//System.out.println("1 doneQ size: " + doneQ.size()); 
+        	if (!foundSolution){
+		        //for(Board b : tempState.board.neighbours(allBoards)) {  
+			    for(Board b : tempState.board.neighbours()) {        							// for each board that's a neighbour of the current board		
+		        	if(tempState.prevState == null || !b.equals(tempState.prevState.board)) {	// board isn't equal to the board for the previous state
+		        		State s = new State(b, tempState);										// create new state	
+		        		boolean boardExpandedAlready = false;
+		        		//if (checkStateBoard(s.board, allBoardsQ)) statePQ.insert(s);	
+		        		
+		        		// DEEP COPY THE QUEUE
+		        		//if (checkStateBoard(s.board)) {
+		        		//Queue<State> checkBoards = new Queue<>(doneQ);
+		        		Queue<State> checkBoards = new Queue<>();
+		        		Iterator<State> iter = doneQ.iterator();
+		        		while(iter.hasNext()){
+		        			checkBoards.enqueue(iter.next());		   // Deep copy the queue     			
+		        		}
+		        		//checkBoards.enqueue(doneQ);		        		
+		        		
+	        			//System.out.println("2 doneQ size: " + doneQ.size());
+		    	    	
+		    	    	while (!checkBoards.isEmpty() && !boardExpandedAlready){
+		    	    		Board checkThis = checkBoards.dequeue().board;	// check each board on the queue
+		    	    		
+		    	    		if(checkThis.boardsAreEqual(checkThis.board, b.board)) {// if they are equal (already on the queue)
+		    	    			boardExpandedAlready = true;						// return false, and don't bother adding it
+		    	    			break;
+		    	    		}
+		    	    	}		
+		    	    	
+	        			System.out.println("3 doneQ size: " + doneQ.size());    	    	
+		    	    	
+		        		if(!boardExpandedAlready) {
+		        			statePQ.insert(s);
+		        			doneQ.enqueue(s);
+		        			//System.out.println("allBoardsQ size: " + allBoardsQ.size());
+		        			//System.out.println("4 doneQ size: " + doneQ.size());
+		        		}
+		        		
+		        		System.out.println("statePQ: " + statePQ.size());
+		        		
+		        		
+		        		if(s.board.boardIsSolution()) {
+		            		solutionState = s;													// set the solution state to as the current state
+		            		foundSolution = true;												// finished / exit function
+		        		}
+		        		//System.out.println("\nQueue size: " + statePQ.size());
+		        	}
+		        }
+        	}
+    	}
+    } 
+
+    //public boolean checkStateBoard(Board b, Queue<Board> allBoards1){
+    public boolean checkStateBoard(Board b){
+		//if (allBoardsQ.isEmpty()) {			
+		if (doneQ.isEmpty()) {							// If the queue of all previous boards is empty, board can be added
+			return true;
+		}
+		else {
+	    	//Queue<Board> checkBoards = allBoardsQ;
+	    	//Queue<State> checkBoards = doneQ;
+	    	Queue<State> checkBoards = new Queue<>();
+		    Iterator<State> blah = doneQ.iterator();
+		    while(blah.hasNext()){
+		    	checkBoards.enqueue(blah.next());
+		    }
+	    	
+	    	while (!checkBoards.isEmpty()){
+	    		Board checkThis = checkBoards.dequeue().board;			// check each board on the queue
+	    		
+	    		if(checkThis.boardsAreEqual(checkThis.board, b.board)) {// if they are equal (already on the queue)
+	    			return false;										// return false, and don't bother adding it
+	    		}
+	    	}
+			//System.out.println("test 3");
+			//if (putOnQueue) 
+			//allBoards.enqueue(b);						// Add the board to the previously visited boards queue				
+		}
+		//System.out.println("allBoards1 size: " + allBoards1.size());
+		//allBoardsQ.enqueue(b);								// Add the board to the previously visited boards queue		
+		//System.out.println("allBoardsQ size: " + allBoardsQ.size());
+		
+    	return true;										// if the board is not already on the boards list, return true to add it
+    }
+       
+    public boolean isSolvable() {
         // YOUR CODE HERE
+    	// Don't need
+    	//return false;
+    	return true; // needs to be solvable
+    }
+        
+    /*
+     * Get the number of moves from initial
+ state to current state
+     */
+    public int moves() {
+        // YOUR CODE HERE
+    	/*
+    	int moves;
+    	State s = solutionState;
+    	if(s.prevState == null) moves = 0;
+    	else moves = s.prevState.moves + 1;
+    	 */
+		State s1 = solutionState;			// Temporary state	
+		//minNumMoves = -1;
+		minNumMoves = 0;					// Reset moves made
+		
+		if (s1.prevState == null){
+			return 0;						// if it's the initial state, moves are 0
+		}
+		else {
+			while(s1.prevState != null) {	// Count back previous states as far as initial null state
+				minNumMoves++;				// Increment moves made
+				s1 = s1.prevState;			// Set to previous state
+			}
+		}
+		
+		return minNumMoves;
     }
     
-    public boolean isSolvable(){
+    /*
+     * Create a list of the board states/tile moves from initial state to goal state
+     */
+    public Iterable<Board> solution() {
         // YOUR CODE HERE
-        return false;
+    	// Stack/queue/priQ/linkedlist?
+    	Queue<Board> anotherQueue = new Queue<Board>();				// Hold the boards
+    	
+    	State trackState = solutionState;							// Make the current state the final state
+    	anotherQueue.enqueue(trackState.board);						// Add the board for the current state to the list of boards
+    	    	
+    	while(trackState.prevState != null){						// Unit the initial state is reached
+    		anotherQueue.enqueue(trackState.prevState.board);		// Add boards to the queue for the previous state
+    		trackState = trackState.prevState;						// Make the current state the previous state
+    	}
+    	
+        return anotherQueue;										// Return the list of boards used to reach the solution
     }
     
-    public int moves(){
-        // YOUR CODE HERE
-        return 0;
-    }
-    
-    public Iterable<Board> solution(){
-        // YOUR CODE HERE
-        return null;
-    }
-    
-    public static void main(String [] args){
-        int N = StdIn.readInt();
+    public static void main(String[] args) {
+    	int N = StdIn.readInt();
         int [][] tiles = new int [N][N];
         
         for (int i = 0; i < N; i++){
@@ -29,6 +216,8 @@ public class Solver{
         }
         
         Board initial = new Board(tiles);
+        
+        //System.out.println("Board Entered: \n" + initial.toString());	// display board entered
         
         Solver solver = new Solver(initial);
         
@@ -41,4 +230,83 @@ public class Solver{
         else
             System.out.println("Mininimum number of moves = " + solver.moves());
     }
+    /*
+     //System.out.println("Board Solution: \n");
+        
+        int[][] test1 = {{8,1,3},{4,0,2},{7,6,5}};
+
+        int[][] test2 = {{1,2,3},{4,5,6},{7,8,0}};
+        
+        int[][] test3 = {{1,2,3},{4,5,6},{7,8,0}};
+        /*
+        for (int i = 0; i < 3; i++){
+        	for (int j = 0; j < 3; j++){
+        		System.out.print(" " + test1[i][j]);
+        		if (j<2) System.out.print(" |");
+        	}
+    		if (i<2) System.out.println("\n---|---|---");
+    		//else System.out.println("\n   |   |   ");
+        }
+        
+
+        //Board blah = new Board(test1);
+        Board blah = new Board(test2);
+
+        //if(blah.boardIsSolution()) System.out.println("\nyes");
+        //else System.out.println("\nno");
+        
+        //Board blah1 = new Board(test2);
+        //if(blah1.boardIsSolution()) System.out.println("\nyes");
+       // else System.out.println("\nno");
+        
+        Solver solver1 = new Solver(blah);
+        
+
+        if (test1 == test2) System.out.println("\nTest1 & Test2 boards are equal");
+        else System.out.println("\nTest1 & Test2 boards are not equal");
+        
+        if (test2 == test3) System.out.println("\nTest2 & Test3 boards are equal");
+        else System.out.println("\nTest2 & Test3 boards are not equal");
+
+
+        for (int i = 0; i < 3; i++){
+        	for (int j = 0; j < 3; j++) {
+        		if(test1[i][j] == test2[i][j]) System.out.println("board[" + i + "][" + j + "] equal");
+        		else System.out.println("board[" + i + "][" + j + "] not equal");
+        	}
+        }
+        
+        
+        //Board blah = new Board(test1);     
+
+        //State testState = new State(blah, null);
+        //testState.moves = 0;
+        
+        //System.out.println("\n\nHamming:\n");
+        //System.out.println("blah hamming: " + blah.hamming());
+        
+        //System.out.println("\nManhattan:\n");
+        //System.out.println("blah manhattan: " + blah.manhattan());
+        
+
+        //System.out.println("\nTest:\n");
+        //blah.neighbours();
+
+    	//System.out.println(statePQ.delMin().toString());
+
+        
+        for (int i = 0; i < 3; i++){
+        	for (int j = 0; j < 3; j++) {
+        		if(test3[i][j] == test2[i][j]) System.out.println("board[" + i + "][" + j + "] equal");
+        		else System.out.println("board[" + i + "][" + j + "] not equal");
+        	}
+        }
+        Vector<Board> v = new Vector<Board>();
+    	//Vector<Board> v = new Vector<Board>();		// Vector of size 20, increases by 10, when out of space
+
+        int[][] test1 = {{8,1,3},{4,0,2},{7,6,5}};
+        Board initial = new Board(test1);
+        
+    	v.add(initial);
+     */
 }
